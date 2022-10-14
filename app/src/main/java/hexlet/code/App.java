d6347d7d50ece27c35d43ea1c9dce9444752daa6
ceptoz <brylin.alex@gmail.com>
2022-10-14 11:25:55 +0400

    Changed menu logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,15 +6,13 @@ import static hexlet.code.Engine.optionStart;
 public class App {
     public static void main(String[] args) {
         String[] menu = {"1 - Greet", "2 - Even", "3 - Calc", "4 - GCD", "5 - Progression", "6 - Prime", "0 - Exit"};
-        int userOption = 2;
+        int userOption;
 
-        while (userOption > 1) {
             System.out.println("Please enter the game number and press Enter.");
             for (String option : menu) {
                 System.out.println(option);
             }
             userOption = enterAnOptionNumber();
             optionStart(userOption);
-        }
     }
 }
